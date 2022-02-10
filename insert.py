dbe7d28e2662830492d92638bd872b35cc6b3b03
Askoou <99373131+Askoou@users.noreply.github.com>
2022-02-10 18:58:55 -0500

    Create insert.py
    Ce code est à insérer dans le code de votre live strategy

---
+++ insert.py
@@ -0,0 +1,34 @@
+                #################################################################
+                AJOUTER ET REMPLIR CETTE PARTIE AVANT LES INDICATORS VARIABLE
+                #################################################################
+                
+                #ID connexion SQL
+                mydb = mysql.connector.connect(
+                  host="",
+                  user="",
+                  password="",
+                  database=""
+                )
+                mycursor = mydb.cursor()
+                
+                
+                
+                #################################################################
+                AJOUTER CETTE PARTIE DANS LA PARTIE # Sell de votre live_strategy
+                #################################################################
+                
+                #Ajout du log SELL dans la db
+                sql = "INSERT INTO orderBook (type, amount, symbol, price) VALUES (%s, %s, %s, %s)"
+                val = ("1", coinBalance[coin], symbol, "0")
+                mycursor.execute(sql, val)
+                mydb.commit()
+                
+                #################################################################
+                AJOUTER CETTE PARTIE DANS LA PARTIE # Buy de votre live_strategy
+                ##################################################################
+                
+                #Ajout du log BUY dans la db
+                sql = "INSERT INTO orderBook (type, amount, symbol, price) VALUES (%s, %s, %s, %s)"
+                val = ("2", buyAmount, symbol, buyPrice)
+                mycursor.execute(sql, val)
+                mydb.commit()
