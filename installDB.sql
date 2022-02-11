76c1ec4c33ecf3e67d4de1d92c76a0ae71e7f438
Askoou <99373131+Askoou@users.noreply.github.com>
2022-02-10 19:04:32 -0500

    Update installDB.sql

--- installDB.sql
+++ installDB.sql
@@ -1,8 +1,25 @@
+CREATE DATABASE  IF NOT EXISTS `cryptobot` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cryptobot`;
 
 DROP TABLE IF EXISTS `afg20`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `afg20` (
   `id` int NOT NULL AUTO_INCREMENT,
   `date` date DEFAULT NULL,
   `wallet` float DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `orderBook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderBook` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` int DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  `symbol` varchar(45) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
