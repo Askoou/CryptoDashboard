e2024c87ec7ea1324fd38dad4a18b5e4cfe0a16a
Askoou <99373131+Askoou@users.noreply.github.com>
2022-02-21 17:44:33 -0500

    Update installDB.sql
    Prise en charge multibot

--- installDB.sql
+++ installDB.sql
@@ -1,20 +1,20 @@
 CREATE DATABASE  IF NOT EXISTS `cryptobot` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `cryptobot`;
 
-DROP TABLE IF EXISTS `afg20`;
+DROP TABLE IF EXISTS `bot1`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `afg20` (
+CREATE TABLE `bot1` (
   `id` int NOT NULL AUTO_INCREMENT,
   `date` date DEFAULT NULL,
   `wallet` float DEFAULT NULL,
   PRIMARY KEY (`id`)
 
 
-DROP TABLE IF EXISTS `orderBook`;
+DROP TABLE IF EXISTS `orderBook1`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `orderBook` (
+CREATE TABLE `orderBook1` (
   `id` int NOT NULL AUTO_INCREMENT,
   `type` int DEFAULT NULL,
   `amount` float DEFAULT NULL,
